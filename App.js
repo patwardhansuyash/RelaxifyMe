4f4debced33b4b1a5e3d95a165bba8727601018d
patwardhansuyash <58746370+patwardhansuyash@users.noreply.github.com>
2020-06-09 22:32:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js').then(()=>{
+        console.log('Service Worker Registerd')
+    })
+}
+
+if('caches' in window){
+    caches.open('relax-data').then((cachex)=>{
+        cachex.addAll([
+            '/App.js',
+            '/undraw_yoga_248n.svg'
+        ])
+    })
+}
+
+document.getElementById('btn').addEventListener('click',(e)=>{
+    if(e.target.innerText==='Start'){
+        document.getElementById('pseudox').classList.add('pseudoactive')
+
+    function NameChange(){
+        setTimeout(()=>{document.getElementById('message').innerText="Breath In ..";}, 2500);
+        setTimeout(()=>{document.getElementById('heart').className='in'}, 2000);
+        setTimeout(()=>{document.getElementById('message').innerText="Hold !";}, 6000);
+        setTimeout(()=>{document.getElementById('message').innerText="Breath out ..";}, 9000);
+        setTimeout(()=>{document.getElementById('heart').className='out'}, 9000);
+        
+    
+    }
+    
+    NameChange();
+    
+    setInterval(()=>NameChange(),13000) 
+    e.target.innerText='Stop'
+    }
+    else{
+        window.location.reload()
+
+    }
+
+})
---
+++ sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install',(e)=>{
+    console.log('Service Worker Installed',e)
+    e.waitUntil(caches.open('Static')
+    .then((cache)=>{
+        cache.addAll([
+            '/',
+            '/index.html',
+            '/style.css'
+            
+        ])
+    }))
+});
+
+self.addEventListener('activate',(e)=>{
+    console.log('Service Worker Activated',e);
+
+});
+
+self.addEventListener('fetch',(e)=>{
+    e.respondWith(
+        fetch(e.request)
+        .catch((err)=>{
+            console.log('could not fetch items',err)
+            return caches.match(e.request)
+        })
+    )
+});
